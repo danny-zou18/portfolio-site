73ca5b874afd56d9c5084fa8bc2cfd655d7ad093
dannyzou18 <dzou3814@gmail.com>
2024-05-24 15:01:47 -0400

    initialized

---
+++ project/src/app/portfolio/page.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const PortfolioPage: React.FC = () => {
+    return (
+        <div>
+        Hi 
+        </div>
+    )
+}
+
+export default PortfolioPage;
