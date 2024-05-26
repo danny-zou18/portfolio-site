2b592f0599d0cc1d2c601ce691d3382385040875
dannyzou18 <dzou3814@gmail.com>
2024-05-26 13:30:52 -0400

    rethemed navbar

--- project/src/app/components/Navbar.tsx
+++ project/src/app/components/Navbar.tsx
@@ -13,7 +13,6 @@ interface NavBtnProp {
 }
 
 const NavBtn: React.FC<NavBtnProp> = ({ href, whereto }) => {
-
   const whileHover = {
     scale: 1.1,
     backgroundColor: "#00000010",
@@ -23,31 +22,44 @@ const NavBtn: React.FC<NavBtnProp> = ({ href, whereto }) => {
   const isActive = pathname === href;
 
   return (
-    <motion.div className="group rounded-sm p-2 ml-10 relative" whileHover={whileHover}>
+    <motion.div
+      className="group rounded-sm p-2 ml-10 relative first:ml-0"
+      whileHover={whileHover}
+    >
       <Link href={href} className="">
         {whereto}
       </Link>
-      <div className="absolute bottom-0 left-0 h-0.5 bg-red-50 transition-all duration-500 group-hover:w-full w-0"></div>
-      <div className={`absolute bottom-0 left-0 h-0.5 bg-red-200 transition-all duration-500 ${isActive ? 'w-full' : ''}`}></div>
+      <div className="absolute bottom-0 left-0 h-0.5 bg-primary transition-all duration-500 group-hover:w-full w-0"></div>
+      <div
+        className={`absolute bottom-0 left-0 h-0.5 bg-primary transition-all duration-500 ${
+          isActive ? "w-full" : ""
+        }`}
+      ></div>
     </motion.div>
   );
 };
 
 const Navbar: React.FC = () => {
-    const contactModal = useContactModal();
+  const contactModal = useContactModal();
 
-    const openContact = useCallback(() => {
-        contactModal.onOpen();
-    }, [contactModal]);
+  const openContact = useCallback(() => {
+    contactModal.onOpen();
+  }, [contactModal]);
   return (
-    <div className="flex flew-row justify-between items-center h-14  bg-gray-500">
+    <div className="flex flew-row justify-between items-center h-14 w-[90%] ml-auto mr-auto">
       <div className="flex flex-row  ">
         <NavBtn href="/" whereto="Home" />
         <NavBtn href="/portfolio" whereto="Portfolio" />
         <NavBtn href="/about" whereto="About" />
-        <button className="pl-2 pr-2 rounded-lg bg-red-200" onClick={openContact}>Contact Me</button>
       </div>
-      <div className="">Hello</div>
+      <div className="">
+        <button
+          className="p-2 rounded-lg bg-primary text-secondary "
+          onClick={openContact}
+        >
+          Contact Me
+        </button>
+      </div>
     </div>
   );
 };
