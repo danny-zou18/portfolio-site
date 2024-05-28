9aa6a3b9ca806a4f9c116c34e50ec263277db5bf
dannyzou18 <dzou3814@gmail.com>
2024-05-27 21:38:21 -0400

    styled featured project grid cards

--- project/src/components/ContactModal.tsx
+++ project/src/components/ContactModal.tsx
@@ -10,7 +10,7 @@ import { useRouter } from "next/navigation";
 import useContactModal from "@/hooks/useContactModal";
 import { IoClose } from "react-icons/io5";
 import { motion } from "framer-motion";
-import { useForm, SubmitHandler } from 'react-hook-form';
+import { useForm, SubmitHandler } from "react-hook-form";
 
 type FormValues = {
   name: string;
@@ -25,26 +25,30 @@ const ContactModal: React.FC = () => {
 
   const [showModal, setShowModal] = useState(contactModal.isOpen);
 
-  const { register, handleSubmit, formState: { errors } } = useForm<FormValues>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
 
   const onSubmit: SubmitHandler<FormValues> = async (data) => {
     try {
-      const response = await fetch('/api/submit', {
-        method: 'POST',
+      const response = await fetch("/api/submit", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(data),
       });
 
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
 
       const result = await response.json();
       console.log(result.message);
     } catch (error) {
-      console.error('Error submitting form:', error);
+      console.error("Error submitting form:", error);
     }
   };
 
@@ -61,52 +65,76 @@ const ContactModal: React.FC = () => {
   };
 
   return (
-    <div
-      className={` border-2 flex flex-col w-[35rem] h-[32rem] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-10 pt-5 shadow-lg rounded-sm ${
-        showModal ? "opacity-100" : "opacity-0"
-      }`}
-    >
-      <motion.div
-        className="close-button absolute top-3 right-3 text-2xl rounded-full p-[0.1rem] cursor-pointer"
-        whileHover={hoverExit}
-        onClick={() => contactModal.onClose()}
+    showModal && (
+      <div
+        className={` border-2 flex flex-col bg-background w-[35rem] h-[32rem] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-10 pt-5 shadow-lg rounded-sm`}
       >
-        <IoClose className="mr-auto ml-auto" />
-      </motion.div>
-      <div className=" border-primary text-[1.7rem]">Contact Me</div>
-      <form className="w-full h-full" onSubmit={handleSubmit(onSubmit)}>
-        <div className="flex justify-between mt-5">
-          <div className="grid w-[48%] max-w-sm items-center gap-1.5">
-            <Label htmlFor="text">Name</Label>
-            <Input type="text" id="name" className="bg-primary text-secondary" {...register('name', { required: true })} />
-            {errors.name && <span>This field is required</span>}
-          </div>
-          <div className="grid w-[48%] max-w-sm items-center gap-1.5">
-            <Label htmlFor="email">Email</Label>
-            <Input type="email" id="email" className="bg-primary text-secondary" {...register('email', { required: true })} />
-            {errors.email && <span>This field is required</span>}
-          </div>
-        </div>
-        <div className="mt-5">
-          <div className="grid w-[48%] max-w-sm items-center gap-1.5">
-            <Label htmlFor="text" className="flex items-center">Subject <span className="text-[.8rem] text-gray-400">(Optional)</span></Label>
-            <Input type="text" id="subject" className="bg-primary text-secondary" {...register('subject', { required: false })} />
-            {errors.subject && <span>This field is required</span>}
-          </div>
-        </div>
-        <div className="grid w-full gap-1.5 mt-5 ">
-          <Label htmlFor="message">Message</Label>
-          <Textarea placeholder="Type your message here." id="message" rows={6} className="bg-primary text-secondary" {...register('message', { required: true })} />
-          {errors.message && <span>This field is required</span>}
-        </div>
-        <button
-          type="submit"
-          className="bg-primary text-background h-[2.5rem] mt-8 rounded-md hover:bg-gray-800 w-full"
+        <motion.div
+          className="close-button absolute top-3 right-3 text-2xl rounded-full p-[0.1rem] cursor-pointer"
+          whileHover={hoverExit}
+          onClick={() => contactModal.onClose()}
         >
-          Submit
-        </button>
-      </form>
-    </div>
+          <IoClose className="mr-auto ml-auto" />
+        </motion.div>
+        <div className=" border-primary text-[1.7rem]">Contact Me</div>
+        <form className="w-full h-full" onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex justify-between mt-5">
+            <div className="grid w-[48%] max-w-sm items-center gap-1.5">
+              <Label htmlFor="text">Name</Label>
+              <Input
+                type="text"
+                id="name"
+                className="bg-primary text-secondary"
+                {...register("name", { required: true })}
+              />
+              {errors.name && <span>This field is required</span>}
+            </div>
+            <div className="grid w-[48%] max-w-sm items-center gap-1.5">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                type="email"
+                id="email"
+                className="bg-primary text-secondary"
+                {...register("email", { required: true })}
+              />
+              {errors.email && <span>This field is required</span>}
+            </div>
+          </div>
+          <div className="mt-5">
+            <div className="grid w-[48%] max-w-sm items-center gap-1.5">
+              <Label htmlFor="text" className="flex items-center">
+                Subject{" "}
+                <span className="text-[.8rem] text-gray-400">(Optional)</span>
+              </Label>
+              <Input
+                type="text"
+                id="subject"
+                className="bg-primary text-secondary"
+                {...register("subject", { required: false })}
+              />
+              {errors.subject && <span>This field is required</span>}
+            </div>
+          </div>
+          <div className="grid w-full gap-1.5 mt-5 ">
+            <Label htmlFor="message">Message</Label>
+            <Textarea
+              placeholder="Type your message here."
+              id="message"
+              rows={6}
+              className="bg-primary text-secondary"
+              {...register("message", { required: true })}
+            />
+            {errors.message && <span>This field is required</span>}
+          </div>
+          <button
+            type="submit"
+            className="bg-primary text-background h-[2.5rem] mt-8 rounded-md hover:bg-gray-800 w-full"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    )
   );
 };
 
--- project/src/components/portfolio/FeaturedProjects.tsx
+++ project/src/components/portfolio/FeaturedProjects.tsx
@@ -95,25 +95,44 @@ const FeaturedProjects: React.FC = () => {
 
   return (
     <motion.div>
-      <div className="grid grid-cols-2 gap-8 w-[70%] h-[30rem] ml-auto mr-auto">
+      <div className="grid grid-cols-2 gap-8 w-[70%] h-[33rem] ml-auto mr-auto">
         {featuredProjects.map((project) => (
-            <motion.div
-              className={`card p-3 bg-white rounded-lg shadow-md cursor-pointer transform transition-transform duration-500 hover:scale-105 ${
-                selectedId === project.id ? 'card-selected' : ''
-              }`}
-              layoutId={`card-container-${project.id}`}
-              onClick={() => setSelectedId(project.id)}
-              key={project.id}
-              initial={{ scale: 1 }}
-              animate={{ scale: selectedId === project.id ? 1.1 : 1 }} // Increase scale on selected card
-              transition={{ duration: 0.3 }}
-            >
-              <div className="card-content">
-                <motion.h2 className="text-xl font-bold mb-2 text-purple-600">{project.title}</motion.h2>
-                <motion.h5 className="text-sm font-bold mb-1 text-gray-700">{project.description}</motion.h5>
-              </div>
-            </motion.div>
-          ))}
+          <motion.div
+            className={`card p-3 pr-2 bg-white rounded-lg shadow-md cursor-pointer transform transition-transform duration-500 hover:scale-105 ${
+              selectedId === project.id ? "card-selected" : ""
+            }`}
+            layoutId={`card-container-${project.id}`}
+            onClick={() => setSelectedId(project.id)}
+            key={project.id}
+            initial={{ scale: 1 }}
+            animate={{ scale: selectedId === project.id ? 1.1 : 1 }} // Increase scale on selected card
+            transition={{ duration: 0.3 }}
+          >
+            <div className="card-content">
+              <motion.h2 className="text-xl font-[300] mb-2 text-background">
+                {project.title}
+              </motion.h2>
+              <motion.div className="flex h-40 ">
+                <motion.div className="border-2 w-[100%] h-40 rounded-lg mr-2"></motion.div>
+                <motion.h5 className="text-sm w-[70%] font-[400] mb-1 text-gray-700">
+                  {project.description}
+                </motion.h5>
+              </motion.div>
+              <motion.div className="flex flex-row mt-2">
+                {project.skills.slice(0, 3).map((skill, index) => (
+                  <div
+                    className="card pl-3 pr-3 pt-1 pb-1 bg-background rounded-xl shadow-md cursor-pointer text-sm mr-2"
+                  >
+                    <h5>{skill}</h5>
+                  </div>
+                ))}
+                <div className="card bg-background rounded-xl shadow-md cursor-pointer pl-2 pr-2">
+                  . . .
+                </div>
+              </motion.div>
+            </div>
+          </motion.div>
+        ))}
       </div>
       <AnimatePresence>
         {selectedId && (
@@ -123,43 +142,49 @@ const FeaturedProjects: React.FC = () => {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
           >
-            {featuredProjects.map((project) => (
-              project.id === selectedId && (
-                <motion.div
-                  className="bg-white rounded-lg p-4 shadow-md max-w-lg mx-auto"
-                  layoutId={`card-container-${project.id}`}
-                  key={project.id}
-                  initial={{ scale: 0.8, opacity: 0 }}
-                  animate={{ scale: 1, opacity: 1 }}
-                  exit={{ scale: 0.8, opacity: 0 }}
-                >
-                  <motion.div className="relative">
-                    <motion.button
-                      className="absolute top-2 right-2 py-1 px-2 text-center text-white bg-red-500 rounded-full"
-                      onClick={() => setSelectedId('')}
-                    >
-                      Close
-                    </motion.button>
-                    <motion.h2 className="text-xl font-bold mb-2 text-purple-600">{project.title}</motion.h2>
-                    <motion.h5 className="text-sm font-bold mb-1 text-gray-700">{project.description}</motion.h5>
-                    <motion.p className="text-md text-gray-700 mb-4">{project.startDate.toISOString()}</motion.p>
-                    <motion.p
-                      className="text-md text-gray-700"
-                      initial={{ opacity: 0 }}
-                      animate={{ opacity: 1 }}
-                      exit={{ opacity: 0 }}
-                    >
-                      Additional content can go here!
-                    </motion.p>
+            {featuredProjects.map(
+              (project) =>
+                project.id === selectedId && (
+                  <motion.div
+                    className="bg-white rounded-lg p-4 shadow-md mx-auto w-[60rem] h-[30rem]"
+                    layoutId={`card-container-${project.id}`}
+                    key={project.id}
+                    initial={{ scale: 0.8, opacity: 0 }}
+                    animate={{ scale: 1, opacity: 1 }}
+                    exit={{ scale: 0.8, opacity: 0 }}
+                  >
+                    <motion.div className="relative">
+                      <motion.button
+                        className="absolute top-2 right-2 py-1 px-2 text-center text-white bg-red-500 rounded-full"
+                        onClick={() => setSelectedId("")}
+                      >
+                        Close
+                      </motion.button>
+                      <motion.h2 className="text-xl font-bold mb-2 text-purple-600">
+                        {project.title}
+                      </motion.h2>
+                      <motion.h5 className="text-sm font-bold mb-1 text-gray-700">
+                        {project.description}
+                      </motion.h5>
+                      <motion.p className="text-md text-gray-700 mb-4">
+                        {project.startDate.toISOString()}
+                      </motion.p>
+                      <motion.p
+                        className="text-md text-gray-700"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                      >
+                        Additional content can go here!
+                      </motion.p>
+                    </motion.div>
                   </motion.div>
-                </motion.div>
-              )
-            ))}
+                )
+            )}
           </motion.div>
         )}
       </AnimatePresence>
     </motion.div>
-    
   );
 };
 
