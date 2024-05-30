4093d851a7853102c0da62a7b14a0a3eb3dc3d46
dannyzou18 <dzou3814@gmail.com>
2024-05-30 12:01:56 -0400

    completed timeline skills for big screens

--- project/src/components/portfolio/timeline/TimelineItem.tsx
+++ project/src/components/portfolio/timeline/TimelineItem.tsx
@@ -1,57 +1,95 @@
-import './Timeline.css'
+import "./Timeline.css";
+
+function LightenDarkenColor(col: string,amt: number) {
+    var usePound = false;
+    if ( col[0] == "#" ) {
+        col = col.slice(1);
+        usePound = true;
+    }
+
+    var num = parseInt(col,16);
+
+    var r = (num >> 16) + amt;
+
+    if ( r > 255 ) r = 255;
+    else if  (r < 0) r = 0;
+
+    var b = ((num >> 8) & 0x00FF) + amt;
+
+    if ( b > 255 ) b = 255;
+    else if  (b < 0) b = 0;
+    
+    var g = (num & 0x0000FF) + amt;
+
+    if ( g > 255 ) g = 255;
+    else if  ( g < 0 ) g = 0;
+
+    return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
+}
+
 
 interface TimelineItemProp {
   data: {
-    text: string,
-    startdate: string,
-    enddate: string,
+    text: string;
+    startdate: string;
+    enddate: string;
     category: {
-      tag: string
-      color: string,
-    },
+      tag: string;
+      color: string;
+    };
     link?: {
-      url: string,
-      text: string,
-    } | null,
-    skills?: string[] | null,
-  }
+      url: string;
+      text: string;
+    } | null;
+    skills?: string[] | null;
+  };
 }
 
-const TimelineItem: React.FC<TimelineItemProp> = ({
-  data
-}) => {
-  return ( 
-    <div className="
+const TimelineItem: React.FC<TimelineItemProp> = ({ data }) => {
+  return (
+    <div
+      className="
       timeline-item text-background
-    ">
-      <div className="timeline-item-content bg-card">
-        <span 
-          className="px-2 rounded-md mb-3 text-xs" 
+    "
+    >
+      <div className="timeline-item-content relative bg-card">
+        <span
+          className="px-2 rounded-md mb-3 text-xs"
           style={{ background: data.category.color }}
         >
           {data.category.tag}
         </span>
-        
-        <time className='text-xs text-accent'>
+
+        <time className="text-xs text-accent">
           {`${data.startdate} - ${data.enddate}`}
         </time>
-        <p className='py-4 text-sm'>
-          {data.text}
-        </p>
+        <p className="py-4 text-sm">{data.text}</p>
+        <div className="skills flex flex-wrap gap-1 max-w-[50%]">
+          {data.skills &&
+            data.skills.map((skill, i) => (
+              <span
+                key={i}
+                className="px-1 rounded-md text-xs "
+                style={{ background: LightenDarkenColor(data.category.color, 60)}}
+              >
+                {skill}
+              </span>
+            ))}
+        </div>
         {data.link && (
           <a
             href={data.link.url}
             target="_blank"
             rel="noopener noreferrer"
-            className='text-xs hover:underline'
+            className="text-xs hover:underline"
           >
-              {`${data.link.text} ►`}
+            {`${data.link.text} ►`}
           </a>
         )}
         <span className="circle" />
       </div>
     </div>
   );
-}
- 
+};
+
 export default TimelineItem;
